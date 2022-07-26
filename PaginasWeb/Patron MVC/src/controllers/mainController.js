f5be375599671bd1e259db1c12782b287c7289e3
Julian Perez <jd.perez998@gmail.com>
2022-07-26 16:24:04 -0500

    delete

--- PaginasWeb/Patron MVC/src/controllers/mainController.js
+++
@@ -1,9 +0,0 @@
-const path = require("path")
-
-let mainController = {
-    ingreso: (req, res) => {
-        res.sendFile(path.join(__dirname, "../views/home.html"))
-    }
-}
-
-module.exports = mainController
